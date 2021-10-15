9b766f218d6ae404cb124e2b435d6dd2b153ba13
ssushmitha52 <sushiii2k1@gmail.com>
2021-10-15 15:50:37 +0530

    Added main app

---
+++ main/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import User
+from jsonfield import JSONField
+
+
+class Therapist(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=100)
+    specialization = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+
+
+class Patient(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    connectionRequests = JSONField()
