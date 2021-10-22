42af24167cb878e9b7f42e997f86fa1e197678fa
ssushmitha52 <sushiii2k1@gmail.com>
2021-10-22 08:22:10 +0530

    added frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+// import React from 'react';
+// import './App.css';
+// import Form from './Form';
+
+// function App() {
+//   return <Form />;
+// }
+
+// export default App;
+
+
+
+import React from 'react';
+import LoginForm from './LoginForm'
+
+function App() {
+  return (
+    <div className="App">
+          <LoginForm />
+    </div>
+  );
+}
+
+export default App;
+
